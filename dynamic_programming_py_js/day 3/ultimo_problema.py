60002695ec8fe248bebb7948b31ef5f16df1f1b5
AngeloMan <angelomandeoliveira@gmail.com>
2025-04-08 23:15:52 -0300

    Add files via upload

---
+++ dynamic_programming_py_js/day 3/ultimo_problema.py
@@ -0,0 +1,46 @@
+from copy import deepcopy
+
+def recive_data(ran = 1):
+    value = []
+    for i in range(ran):
+        value.append([int(x) for x in str(input()).split()])
+    return value
+
+def check_trail(f, t, conections, n, x = 0, route='', routes_ = []):
+    if x < n - 1:
+        for i in conections:
+            if f in i:
+                route += str(f)
+                if t in i:
+                    route += str(t)
+                    routes_.append(route)
+                    route = ''
+                else:
+                    for k in i:
+                        if k != f:
+                            x+=1
+                            check_trail(k, t, n, x, route)
+                    
+        return routes_
+    else:
+        return None
+    
+
+if __name__ == "__main__":
+    n, b = recive_data()[0]
+    barcos = recive_data(b)
+    conections = deepcopy(barcos)
+    
+    for i in conections:
+        i.pop(2)
+    
+    # b1-b2 np
+    n_cases = int(input())
+    cases = recive_data(n_cases)
+    
+    a = []
+    
+    for i in cases:
+        a.append(check_trail(i[0], i[1]))
+        
+    print(a)
