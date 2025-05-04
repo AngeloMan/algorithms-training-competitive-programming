5de4947786a2f6d3a9f4558c7c9f9e634cd2e73b
Angelo Man <angelomandeoliveira@gmail.com>
2025-05-04 01:11:21 -0300

    Update spiralOrder.js

--- leetcode/dia 028/spiralOrder.js
+++ leetcode/dia 028/spiralOrder.js
@@ -6,27 +6,19 @@ var spiralOrder = function(matrix) {
             check.push(i.toString() + '-' + j.toString());
         }
         if (d === 0){
-            if (check.includes(i.toString() + '-' + (j + 1).toString()) || j + 1 > matrix[0].length - 1){
-                d = 1;
-            }
+            if (check.includes(i.toString() + '-' + (j + 1).toString()) || j + 1 > matrix[0].length - 1) d = 1;
             else j++;
         }
         else if (d === 1){
-            if (check.includes((i - 1).toString() + '-' + j.toString())|| i - 1 < 0){
-                d = 2;
-            }
+            if (check.includes((i - 1).toString() + '-' + j.toString())|| i - 1 < 0) d = 2;
             else i--;
         }
         else if (d === 2){
-             if (check.includes(i.toString() + '-' + (j - 1).toString())|| j - 1 < 0){
-                d = 3;
-            }
+             if (check.includes(i.toString() + '-' + (j - 1).toString())|| j - 1 < 0) d = 3;
             else j--;
         }
         else if (d === 3){
-            if (check.includes((i + 1).toString() + '-' + j.toString()) || i + 1 > matrix.length - 1){
-                d = 0;
-            }
+            if (check.includes((i + 1).toString() + '-' + j.toString()) || i + 1 > matrix.length - 1) d = 0;
             else i++;
         }
     }
