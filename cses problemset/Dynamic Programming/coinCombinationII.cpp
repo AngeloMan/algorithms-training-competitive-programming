d04103d11fc0c00921a01ca5b4be2d515e00a7b5
Angelo Man <angelomandeoliveira@gmail.com>
2025-07-19 11:15:18 -0300

    Update coinCombinationII.cpp

--- cses problemset/Dynamic Programming/coinCombinationII.cpp
+++ cses problemset/Dynamic Programming/coinCombinationII.cpp
@@ -1,1 +1,23 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int mod = 1e9 + 7;
+
+int main(){
+
+	int n, s;
+	cin >> n >> s;
+	vector <int> coins(n);
+	for (int i = 0; i < n; i ++) cin >> coins[i];
+	vector <int> dp(s + 1, 0);
+	dp[0] = 1;
+	sort(coins.begin(), coins.end());
+  for (int c = 0; c < n && i >= coins[c]; c++){
+	  for (int i = 1; i <= s; i ++){
+			dp[i] = (dp[i] + dp[i - coins[c]])%mod;
+		}
+	}
+	cout << dp[s];
+
+}
