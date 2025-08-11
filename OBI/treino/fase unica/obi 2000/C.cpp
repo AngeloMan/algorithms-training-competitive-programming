66efb621ade6ff5e82c8667af840ff6b83a7030d
Angelo Man <angelomandeoliveira@gmail.com>
2025-08-10 23:11:46 -0300

    Update C.cpp

--- OBI/treino/fase unica/obi 2000/C.cpp
+++ OBI/treino/fase unica/obi 2000/C.cpp
@@ -8,14 +8,12 @@ int main(){
 		q++;
 		cin >> n;
 		if (n == 0) break;
-		a = n/50;
-		n-= 50 * a;
-		b = n/10;
-		n -= 10 * b;
-		c = n/5;
-		n -= c * 5;
+		a = n/50; n-= 50 * a;
+		b = n/10; n -= 10 * b;
+		c = n/5; n -= c * 5;
 		d = n;
 		cout << "Teste " << q << "\n";
 		cout << a << " " << b << " " << c << " " << d << "\n";  		
 	}
+
 }
